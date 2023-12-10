6b335207c8bc82130dcc1e335605e8d378a5c28a
Michal Zielinski <zielinski93.m@gmail.com>
2023-12-10 21:08:21 +0100

    Project setup

---
+++ src/main/java/com/cryptrade/CryptradeApplication.java
@@ -0,0 +1,13 @@
+package com.cryptrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptrade/CryptradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
